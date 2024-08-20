ba990730ccc3ffee8802c2d3e7cccbcc30129302
safia bano <safiabano736@gmail.com>
2024-08-20 17:07:27 +0500

    assignment4-5

---
+++ app.js
@@ -0,0 +1,119 @@
+// CH-4 Start===========
+
+// Q1
+
+// let myname ,age, dob;
+
+// Q2
+
+// legal
+// document.write("Legal: small, camelCase, snake_case, my22name, massege33"+"<br>"+"<br>") 
+// illegal
+// document.write("Illigal: Start-number, space , minus-sign , capital, Predefined variable name ") 
+
+// CH 4 Complete================
+
+
+// CH 5 Math Expressions start================
+
+// Q1
+
+// let stg = "sum of 3 and 5 is "
+// let sum = 3 + 5
+// document.write(stg+sum)
+ 
+// Q2
+
+// let stg = "subtraction of 3 and 5 is "
+// let sum = 3 - 5
+// document.write(stg+sum)
+// =======================
+// let stg = "Divide of 3 and 5 is "
+// let sum = 3 / 5
+// document.write(stg+sum)
+// ====================
+// let stg = "Modulus of 5 and 3 is "
+// let sum = 5 % 3
+// document.write(stg+sum)
+// Q3
+
+// let num ;
+// document.write("Value after variable declaration is "+ num+"<br>")
+// num = 5;
+// document.write("Initial value "+ num +"<br>")
+// num++
+// document.write("Value after increament is "+ num+"<br>")
+//  num = num + 7
+// document.write("Value after addition is "+ num+"<br>")
+// --num
+// document.write("Value after decreament is "+ num+"<br>")
+// document.write("The reminder is "+3 / num)
+
+// Q4
+
+// let movieTicket = 600;
+// let sold = 5;
+// document.write("total cost to buy "+sold+ " tickets to a movie is "+sold*movieTicket+" PKR")
+// Q5
+
+// let table =prompt("Enter your number")
+// for(let i =1 ; i <= 10; i++){
+//     document.write(table + " x " +i +" = " + table*i+"<br>")
+// }
+// Q6
+
+// let item1 =3 ;
+// let item1Price = 650;
+// let item2 =7;
+// let item2Prce =100;
+// let shippingCh = 100
+// let add = (item1*item1Price)+ (item2*item2Prce)+shippingCh
+// document.write("Price of item 1 is "+ item1Price+"<br>")
+// document.write("Quantity of item 1 is "+ item1+"<br>")
+// document.write("Price of item 2 is "+ item2Prce+"<br>")
+// document.write("Quantity of item 2 is "+ item2+"<br>")
+// document.write("Shipping charges "+ shippingCh +"<br>"+"<br>")
+// document.write("Total cost of your order is "+ add)
+// Q7
+// let TotalMarks = 980;
+// let obtMa = 804;
+// document.write("Total Marks is :"+TotalMarks+"<br>")
+// document.write("Obtained Marks :"+ obtMa +"<br>")
+// document.write("Percentage :"+obtMa/TotalMarks*100 +"%")
+// Q8
+// let usDollar =10;
+// let saudiRiyal =25;
+// let pakisDoll = 104;
+// let saudiPakis = 28;
+// let exchangMultiply= usDollar*pakisDoll+saudiRiyal*saudiPakis
+// document.write("<h1>Currency in PKR: <h1/>")
+// document.write("Total currency in PKR: "+exchangMultiply)
+// Q9
+// let num = ((2+5)*10)/2;
+// document.write(num)
+
+// Q10
+
+// document.write("<h1>Age Calculator</h1>")
+
+// let currentYear = +prompt("enter current year")
+// let dob = +prompt("enter your birth year")
+// let sum = currentYear - dob
+// document.write("CurrentYear is "+ currentYear+ "<br>");
+// document.write("Date of birth is "+ dob +"<br>");
+// document.write("Your age is "+ sum);
+// Q11
+
+// let favSnacks = prompt("Enter your favourite snack","chocolate chip")
+// let currAge = +prompt("Enter your age" ,"15")
+// let estimatMax = +prompt("Enter your estimated age","65")
+// let perDayAmount = +prompt("Enter your per day amount","3")
+// let calculate = (estimatMax-currAge)*3
+// console.log("Favourite snack is "+ favSnacks);
+// console.log("Current age :"+ currAge);
+// console.log("Max Estimated Age :"+ estimatMax);
+// console.log("Amount of snacks per day :"+perDayAmount);
+// console.log("You will need "+ calculate+ " " +favSnacks+" to last you until the ripe old age of " +estimatMax);
+
+
+// CH 5 complete ===========
